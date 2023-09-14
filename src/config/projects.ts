39ea69539f78ddbc71064f38e79d9ba66d5c946a
Anthony Amaro <anthonyamaro@Anthonys-MacBook-Pro.local>
2023-09-13 18:52:33 -0700

    feat: add first projects

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -1,26 +1,29 @@
 import portfolioImg from "../../public/portfoliov1.png";
+import iucnImg from "../assets/iucn.png";
 
 export const PROJECTS = [
   {
     id: "portfolio-v1",
-    name: "Portfolio v1",
+    name: "Portfolio version one",
     headerDescription: "My first portfolio website",
-    description:
-      "My first portfolio website built with HTML, CSS, and JavaScript. I built this website to showcase my projects and experience.",
-    technologies: ["HTML", "CSS", "JavaScript"],
-    website: "www.google.com",
-    github: "www.github.com",
+    description: "I built this website to showcase my projects and experience.",
+    technologies: ["React", "SCSS", "JavaScript"],
+    website: "https://glowing-starburst-c9b86c.netlify.app/",
+    github: "https://github.com/anthonyamaro15/portfolio",
     image: portfolioImg,
+    imgAlt: "Portfolio v1 main page",
   },
   {
     id: "iucn",
-    name: "iucn",
-    headerDescription: "My first portfolio website",
+    name: "IUCN Contributions for Nature",
+    headerDescription:
+      "Online tool quantifies contributions to global goals for nature",
     description:
-      "My first portfolio website built with HTML, CSS, and JavaScript. I built this website to showcase my projects and experience.",
-    technologies: ["HTML", "CSS", "JavaScript"],
-    website: "www.google.com",
-    github: "www.github.com",
-    image: portfolioImg,
+      "IUCN (International Union for Conservation of Nature) presents the IUCN Contributions for Nature Platform. This online tool provides governments, civil society and Indigenous Peoples Organisations with a simple way to measure potential contributions from specific actions in specific places towards global biodiversity and climate goals.",
+    technologies: ["React", "TypeScript", "SCSS", "Arcgis SDK JS"],
+    website: "https://www.iucncontributionsfornature.org/",
+    github: null,
+    image: iucnImg,
+    imgAlt: "IUCN Contributions for Nature",
   },
 ];
